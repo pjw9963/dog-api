a3249cc85b37c090ddaff9a34ca4248e3a538d55
Patrick Wilbur <pjw9963@rit.edu>
2022-10-09 12:41:01 -0400

    implemented controller, duh

---
+++ src/Controllers/DogImageController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+
+using dog_api.Repositories;
+
+namespace dog_api.Controllers;
+
+[Route("api/files")]
+[ApiController]
+public class DogImageController : ControllerBase    //TODO add error handling
+{
+    private readonly IDogImageRepo _dogImageRepo;
+
+    public DogImageController(IDogImageRepo dogImageRepo)
+    {
+        _dogImageRepo = dogImageRepo;
+    }
+
+    [HttpPost("upload")]
+    public async Task<IActionResult> UploadFileAsync(IFormFile file)
+    {
+        var id = await _dogImageRepo.Add(file);
+        return Ok($"File {id} uploaded to S3 successfully.");
+    }
+
+    [HttpGet("get-stream-by-key")]
+    public async Task<IActionResult> GetFileByKeyAsync(string key)
+    {
+        var file = await _dogImageRepo.GetDogImageStream(new Guid(key));
+        return File(file.File.Stream, file.File.ContentType);
+    }
+
+    [HttpDelete("delete")]
+    public async Task<IActionResult> DeleteFileAsync(string key)
+    {
+        await _dogImageRepo.Delete(new Guid(key));
+        return NoContent();
+    }
+}
