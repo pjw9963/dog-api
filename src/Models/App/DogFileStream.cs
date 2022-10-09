ad4eeb81c17aa9d14d0017db4291ba5726b80928
Patrick Wilbur <pjw9963@rit.edu>
2022-10-09 12:40:48 -0400

    unmarked as nullable, need to fix warnings still

--- src/Models/App/DogFileStream.cs
+++ src/Models/App/DogFileStream.cs
@@ -2,6 +2,6 @@ namespace dog_api.Models.App;
 
 public class DogFileStream
 {
-    public Stream? Stream { get; set; }
-    public string? ContentType { get; set; }
+    public Stream Stream { get; set; }
+    public string ContentType { get; set; }
 }
